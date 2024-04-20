848d6792be2f5ed55afd8bb13a0ceeeebf5b40bc
Waleed Gudah <aquataze@yahoo.com>
2024-04-20 00:40:56 -0500

    Updated server history, main page, and server script; added new stylesheet

--- src/server.ts
+++ src/server.ts
@@ -28,25 +28,28 @@
  *    # Optional - you can use the path or a json object
  *    # GMAIL_MAILER_SERVICE_ACCOUNT={"type":"service_account","project_id":"...","private_key_id":"...","private_key":"-----BEGIN PRIVATE KEY-----\\n...\\n-----END PRIVATE KEY-----\\n","client_email":"....iam.gserviceaccount.com","client_id":"...","auth_uri":"...","token_uri":"...","auth_provider_x509_cert_url":"...","client_x509_cert_url":"..."}
  */
-import express from 'express';
-import { ISendEmailResponse } from 'gmail-node-mailer/dist/types';
-import { sendHtmlEmail } from './examples/sendHtmlEmail';
-import { sendPlainTextEmail } from './examples/sendPlainTextEmail';
-import { sendNewPurchaseEmail } from './examples/sendNewPurchaseEmail';
-import { sendServerStatusEmail } from './examples/sendServerStatusEmail';
-import { initializeEmailClient } from './init/initializeEmailClient';
-import { sendHtmlEmailWithAttachment } from './examples/sendHtmlEmailWithAttachment';
-import { sendSubscriptionRenewalEmail } from './examples/sendSubscriptionRenewalEmail';
-import opener from 'opener';
 
 require('dotenv-flow').config();
 
+ import express from 'express';
+ import opener from 'opener';
+ import { ISendEmailResponse } from 'gmail-node-mailer/dist/types';
+ 
+ import { sendHtmlEmail } from './examples/sendHtmlEmail';
+ import { sendPlainTextEmail } from './examples/sendPlainTextEmail';
+ import { sendNewPurchaseEmail } from './examples/sendNewPurchaseEmail';
+ import { sendServerStatusEmail } from './examples/sendServerStatusEmail';
+ import { initializeEmailClient } from './init/initializeEmailClient';
+ import { sendHtmlEmailWithAttachment } from './examples/sendHtmlEmailWithAttachment';
+ import { sendSubscriptionRenewalEmail } from './examples/sendSubscriptionRenewalEmail';
+ 
 declare global {
     var gmailClient: any;
 }
 
 const PORT = process.env.DEFAULT_URL ? parseInt(process.env.DEFAULT_URL.split(':').pop() as string) || 6338 : 6338;
 const app = express();
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/files', express.static('dummyFiles'));
@@ -54,22 +57,20 @@ app.use(express.static('public')); // Serve static files from 'public' directory
 
 function setupEmailEndpoint(path: string, emailFunction: () => Promise<ISendEmailResponse>) {
     app.get(path, async (req, res) => {
-        const result: ISendEmailResponse = await emailFunction();
+        const result = await emailFunction();
         res.json({
             path: path.replace('/', '').replace(/-/g, ' ').toUpperCase(),
             sent: result.sent,
             status: result.status || 'N/A',
             statusText: result.statusText || 'No status text',
-            message: result.message || 'No message provided'
+            message: result.message || 'No message provided',
         });
     });
 }
 
 // Setup email endpoints
-initializeEmailClient().then(emailClientResult => {
+initializeEmailClient().then((emailClientResult: { gmailClient: any; }) => {
     global.gmailClient = emailClientResult.gmailClient;
-
-    setupEmailEndpoint('/simulate-server-status', () => sendServerStatusEmail('start'));
     setupEmailEndpoint('/send-html-email', sendHtmlEmail);
     setupEmailEndpoint('/send-plain-text-email', sendPlainTextEmail);
     setupEmailEndpoint('/send-html-email-attachment', sendHtmlEmailWithAttachment);
@@ -77,15 +78,30 @@ initializeEmailClient().then(emailClientResult => {
     setupEmailEndpoint('/send-new-purchase', sendNewPurchaseEmail);
 });
 
-const server = app.listen(PORT, async () => {
-    console.log(`[Gmail-Node-Mailer Test Server] - Server is listening on port: ${PORT}`);
-    try {
-        await opener(`http://localhost:${PORT}`);
-    } catch (error) {
-        console.error('Failed to open browser:', error);
-    }
+// Endpoint to simulate server start and stop notifications
+app.get('/simulate-server-status', async (req, res) => {
+    console.log('[Demo] Simulating server start...');
+    const startEmailResult: ISendEmailResponse = await sendServerStatusEmail('start');
+    console.log('Server Start Email Send Result:', startEmailResult.sent);
+
+    console.log('[Demo] Simulating server shutdown...');
+    const stopEmailResult: ISendEmailResponse = await sendServerStatusEmail('shutdown');
+    console.log('Server Shutdown Email Send Result:', stopEmailResult.sent);
+
+    // Sending both results back as an array of results
+    res.json([
+        { action: 'Server Start', ...startEmailResult },
+        { action: 'Server Shutdown', ...stopEmailResult }
+    ]);
 });
 
+// Open browser window on server start
+const server = app.listen(PORT, () => {
+    console.log(`[Gmail-Node-Mailer Test Server] - Server is listening on port: ${PORT}`);
+    opener(`http://localhost:${PORT}`);
+});
+
+// Graceful shutdown
 process.on('SIGINT', () => {
     console.log('Server is shutting down...');
     server.close(() => {
