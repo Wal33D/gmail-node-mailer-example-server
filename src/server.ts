c7e903489347bf7aa3470ccc26f5ec3cce76c0c9
Waleed Gudah <aquataze@yahoo.com>
2024-04-19 19:35:40 -0500

    Updated ServerHistory.csv and server.ts with recent changes

--- src/server.ts
+++ src/server.ts
@@ -1,51 +1,43 @@
 /**
- * Module: Test Server for Gmail-Node-Mailer
+ * Demonstration Module: gmail-node-mailer npm package Usage Examples
  *
- * This module initializes and runs an Express server to demonstrate the capabilities of the 'gmail-node-mailer' package by sending various types of emails.
+ * This Express server application serves as a live demonstration platform for the 'gmail-node-mailer' package, showcasing various email sending scenarios using a simulated server environment.
  *
- * Features:
- *  - Initialize the Gmail client globally and set up middleware for handling JSON and URL-encoded data.
- *  - Serve static files located in the 'dummyFiles' directory.
- *  - Automatically send an email notification when the server starts or is shut down using Ctrl+C.
- *  - Demonstrate various email sending functionalities, including HTML emails, plain text emails, emails with attachments, and specific use-case emails like subscription renewals and new purchase confirmations.
+ * Demonstrations Include:
+ *  - Email Types:
+ *    - HTML Emails: Sends a stylized HTML email to illustrate how to include HTML content and styles in emails.
+ *    - Plain Text Emails: Demonstrates sending simple text-based emails for scenarios where simplicity is key.
+ *    - Emails with Attachments: Shows how to attach files (like PDFs and text files) to emails, suitable for invoices, reports, or promotional materials.
+ *  - Sample Use Cases:
+ *    - Subscription Renewal Emails: Sends detailed renewal confirmations with attachments, showcasing the package's utility in subscription-based services.
+ *    - New Purchase Confirmations: Sends purchase confirmation emails, highlighting how to enhance customer purchase experiences with immediate confirmation.
+ *    - Server Status Notifications: Automatically sends an email when the server starts or shuts down, demonstrating the package's capability to integrate with server lifecycle events.
  *
- * Global Variables:
- *  - `gmailClient`: Instance of the GmailMailer, initialized and used across the server for sending emails.
+ * This server is particularly useful for developers looking to integrate email functionalities in their applications using 'gmail-node-mailer', providing practical, executable examples.
  *
- * Environment Variables:
- *  - `DEFAULT_URL`: Determines the port number on which the server listens. If not set, defaults to 6338.
+ * Note:
+ *  - The server requires certain environment variables to be set for proper configuration and operation.
+ *  - Below is a sample `.env` configuration for setting up the server and email functionalities:
  *
- * Endpoints:
- *  - `/files`: Serves static content from the 'dummyFiles' directory.
+ *    # === Server URL/PORT ===
+ *    DEFAULT_URL=http://localhost:6338
  *
- * Email Sending Examples Demonstrated:
- *  - sendServerStatusEmail: Sends a notification on server start or shutdown.
- *  - sendHtmlEmail: Sends a stylized HTML email for service notifications.
- *  - sendPlainTextEmail: Sends a simple text-based email.
- *  - sendHtmlEmailWithAttachment: Sends an HTML email with file attachments.
- *  - sendSubscriptionRenewalEmail: Sends a detailed HTML email for subscription renewal.
- *  - sendNewPurchaseEmail: Sends a confirmation email for new purchases.
- *
- * Usage:
- *  - The server can be started by running this file. Email sending functions are called within the server lifecycle hooks and upon specific endpoint hits.
- *
- * Shutdown Handling:
- *  - Graceful shutdown is implemented to send a shutdown email notification and properly close the server on receiving SIGINT.
- *
- * @returns {void} The server runs indefinitely until manually stopped, handling incoming requests and sending emails based on predefined triggers.
+ *    # GmailMailer Config
+ *    GMAIL_MAILER_SENDER_EMAIL=no-reply@somnuslabs.com
+ *    GMAIL_MAILER_SERVICE_ACCOUNT_PATH=.\private\serviceAccount.json
+ *    # Optional - you can use the path or a json object
+ *    # GMAIL_MAILER_SERVICE_ACCOUNT={"type":"service_account","project_id":"...","private_key_id":"...","private_key":"-----BEGIN PRIVATE KEY-----\\n...\\n-----END PRIVATE KEY-----\\n","client_email":"....iam.gserviceaccount.com","client_id":"...","auth_uri":"...","token_uri":"...","auth_provider_x509_cert_url":"...","client_x509_cert_url":"..."}
  */
-
 require('dotenv-flow').config();
 
 import express from 'express';
 
 import { ISendEmailResponse } from 'gmail-node-mailer/dist/types';
 import { initializeEmailClient } from './init/initializeEmailClient';
-import * as examples from './examples'; // Importing from examples index
-
+import * as examples from './examples'; // Importing example functions to demonstrate email capabilities
 
 declare global {
-    var gmailClient: any; // Global declaration for the Gmail client
+    var gmailClient: any; // Global declaration to share the Gmail client across the demonstration modules
 }
 
 const PORT = process.env.DEFAULT_URL
@@ -55,32 +47,31 @@ const PORT = process.env.DEFAULT_URL
 (async () => {
     const app = express();
 
-    // Initialize the Gmail client and set it as a global variable
+    // Initialize the Gmail client as a demonstration of setting up the 'gmail-node-mailer' package for email operations
     const emailClientResult = await initializeEmailClient();
     global.gmailClient = emailClientResult.gmailClient;
 
-    // Middleware to parse URL-encoded data and JSON
+    // Middleware setup for handling form submissions and JSON data as part of the email sending demonstrations
     app.use(express.urlencoded({ extended: true }));
     app.use(express.json());
 
-    // Static file serving for downloadable content in the dummyFiles directory
+    // Serve static files as part of demonstrating attachment handling in email sending
     app.use('/files', express.static('dummyFiles'));
 
-    // Send a notification email when the server starts
+    // Demonstration of sending a notification email upon server start
     const serverStartResult: ISendEmailResponse = await examples.sendServerStatusEmail('start');
     console.log('Server Start Email Send Result:', serverStartResult.sent);
 
-    // Start the server on the specified port and log the initialization summary
+    // Logs server startup for demonstration purposes
     const server = app.listen(PORT, () => {
         console.log('[Gmail-Node-Mailer Test Server] - Initialization Summary:');
         console.log('Server is listening on port:', PORT);
     });
 
-    // Setup graceful shutdown handling when receiving SIGINT (Ctrl+C)
+    // Demonstrates graceful shutdown and notification via email on SIGINT
     process.on('SIGINT', async () => {
-        // Notify about server shutdown via email
-        const serverStartResult: ISendEmailResponse = await examples.sendServerStatusEmail('shutdown');
-        console.log('Server Shutdown Email Send Result:', serverStartResult.sent);
+        const shutdownEmailResult: ISendEmailResponse = await examples.sendServerStatusEmail('shutdown');
+        console.log('Server Shutdown Email Send Result:', shutdownEmailResult.sent);
 
         server.close(() => {
             console.log('HTTP server closed.');
@@ -88,20 +79,9 @@ const PORT = process.env.DEFAULT_URL
         });
     });
 
-    // Demonstrate various email sending functionalities
-    const sendHTMLEmailResult = await examples.sendHtmlEmail();
-    console.log('Service Notification Email Send Result:', sendHTMLEmailResult.sent);
+    // Demonstrates sending various types of emails using the 'gmail-node-mailer' package
+    const sendHTMLEmailResult:ISendEmailResponse = await examples.sendHtmlEmail();
+    console.log('Service Notification Email Send Result:', sendHTMLEmailResult);
 
-    const plainTextEmailResult: ISendEmailResponse = await examples.sendPlainTextEmail();
-    console.log('Plain Text Email Send Result:', plainTextEmailResult.sent);
-
-    const htmlEmailWithAttachmentResult:ISendEmailResponse = await examples.sendHtmlEmailWithAttachment();
-    console.log('HTML Email with Attachment Send Result:', htmlEmailWithAttachmentResult.sent);
-
-    const sendSubscriptionRenewalResult:ISendEmailResponse = await examples.sendSubscriptionRenewalEmail();
-    console.log(`Send Subscription Renewal Email Result:`, sendSubscriptionRenewalResult.sent);
-
-    const sendNewPurchaseResult:ISendEmailResponse = await examples.sendNewPurchaseEmail();
-    console.log(`Send New Purchase Email Result:`, sendNewPurchaseResult.sent);
 
 })();
